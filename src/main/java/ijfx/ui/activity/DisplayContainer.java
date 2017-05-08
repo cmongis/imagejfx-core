60b89fa8c709e164ad78008cb623b7ca07ef5356
cyril <cyril@129.206.96.102>
2017-05-08 09:58:29 +0200

    Added the activity that will contain ImageJ Displays

---
+++ src/main/java/ijfx/ui/activity/DisplayContainer.java
@@ -0,0 +1,60 @@
+/*
+    This file is part of ImageJ FX.
+
+    ImageJ FX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ImageJ FX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ImageJ FX.  If not, see <http://www.gnu.org/licenses/>. 
+    
+     Copyright 2015,2016 Cyril MONGIS, Michael Knop
+	
+ */
+package ijfx.ui.activity;
+
+import ijfx.core.activity.Activity;
+import javafx.concurrent.Task;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import jfxtras.scene.control.window.Window;
+import org.scijava.plugin.Plugin;
+
+/**
+ *
+ * @author cyril
+ */
+@Plugin(type = Activity.class,name = "display container")
+public class DisplayContainer  extends StackPane implements Activity{
+
+    
+    public AnchorPane anchorPane = new AnchorPane();
+
+    public DisplayContainer() {
+        getChildren().add(anchorPane);
+    }
+    
+    
+            
+    public void addWindow(Window window) {
+        anchorPane.getChildren().add(window);
+    }        
+            
+    @Override
+    public Node getContent() {
+        return this;
+    }
+
+    @Override
+    public Task updateOnShow() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
