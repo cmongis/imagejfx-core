ef40417d31b904b068a43036b7da06529c38539a
cyril <cyril@129.206.96.102>
2017-05-10 14:31:47 +0200

    Added whole class stack to dipslay Explorables

---
+++ src/main/java/ijfx/explorer/display/DefaultExplorableDisplay.java
@@ -0,0 +1,93 @@
+/*
+    This file is part of ImageJ FX.
+
+    ImageJ FX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ImageJ FX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ImageJ FX.  If not, see <http://www.gnu.org/licenses/>. 
+    
+     Copyright 2015,2016 Cyril MONGIS, Michael Knop
+	
+ */
+package ijfx.explorer.display;
+
+import ijfx.explorer.ExplorableDisplay;
+import ijfx.explorer.ExplorableList;
+import ijfx.explorer.datamodel.Explorable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import org.scijava.display.AbstractDisplay;
+import org.scijava.display.Display;
+import org.scijava.plugin.Plugin;
+
+/**
+ *
+ * @author cyril
+ */
+@Plugin(type = Display.class)
+public class DefaultExplorableDisplay extends AbstractDisplay<ExplorableList> implements ExplorableDisplay {
+
+    public DefaultExplorableDisplay() {
+        super(ExplorableList.class);
+    }
+    
+    List<Explorable> displayedItems = new ArrayList<>();
+
+    List<Explorable> items = new ArrayList<>();
+    
+    @Override
+    public boolean add(ExplorableList list) {
+        return items.addAll(list);
+    }
+  
+
+    @Override
+    public List<Explorable> getDisplayedItems() {
+        return displayedItems;
+    }
+
+    @Override
+    public void setFilter(Predicate<Explorable> filter) {
+        if(filter == null) {
+            displayedItems.clear();
+            displayedItems.addAll(items);
+        }
+        else {
+            displayedItems.clear();
+            displayedItems = displayedItems
+                    .stream()
+                    .filter(filter)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Override
+    public List<Explorable> getSelected() {
+        return items
+                .stream()
+                .filter(Explorable::isSelected)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void setSelected(List<Explorable> explorable) {
+        
+        clearSelection();
+        
+    }
+    
+    private void clearSelection() {
+        
+    }
+
+}
---
+++ src/main/java/ijfx/explorer/display/ExplorableDisplayPanel.java
@@ -0,0 +1,115 @@
+/*
+    This file is part of ImageJ FX.
+
+    ImageJ FX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ImageJ FX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ImageJ FX.  If not, see <http://www.gnu.org/licenses/>. 
+    
+     Copyright 2015,2016 Cyril MONGIS, Michael Knop
+	
+ */
+package ijfx.explorer.display;
+
+import ijfx.core.icon.FXIconService;
+import ijfx.core.utils.SciJavaUtils;
+import ijfx.explorer.ExplorableDisplay;
+import ijfx.explorer.views.ExplorerView;
+import ijfx.ui.display.image.AbstractFXDisplayPanel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.PluginService;
+
+/**
+ *
+ * @author cyril
+ */
+public class ExplorableDisplayPanel extends AbstractFXDisplayPanel<ExplorableDisplay> {
+
+    AnchorPane root;
+
+    @Parameter
+    PluginService pluginService;
+
+    @Parameter
+    FXIconService fxIconService;
+
+    TabPane tabPane;
+
+    List<ExplorerView> viewList = new ArrayList<>();
+
+    public ExplorableDisplayPanel() {
+        super(ExplorableDisplay.class);
+    }
+
+    @Override
+    public void pack() {
+
+        root = new AnchorPane();
+
+        tabPane = new TabPane();
+
+        root.getChildren().add(tabPane);
+
+    }
+
+    private Tab createTab(ExplorerView view) {
+
+        Tab tab = new Tab();
+        tab.setContent(view.getUIComponent());
+        tab.setGraphic(root);
+        tab.setUserData(view);
+        tab.setText(SciJavaUtils.getLabel(view));
+        tab.setGraphic(fxIconService.getIconAsNode(view));
+        return tab;
+
+    }
+
+    @Override
+    public Pane getUIComponent() {
+        return root;
+    }
+
+    @Override
+    public void redoLayout() {
+
+        tabPane.getTabs().clear();
+        viewList = pluginService
+                .createInstancesOfType(ExplorerView.class);
+        tabPane.getTabs()
+                .addAll(
+                        viewList
+                                .stream()
+                                .map(this::createTab)
+                                .collect(Collectors.toList()));
+
+    }
+
+    @Override
+    public void setLabel(String s) {
+    }
+
+    @Override
+    public void redraw() {
+        viewList
+                .stream()
+                .forEach(view->{
+                    view.setItem(getDisplay().getDisplayedItems());
+                });
+    }
+
+}
