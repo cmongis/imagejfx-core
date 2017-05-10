57dfdf0ee8e13fbe96f434da0b4c9d7210c1b328
cyril <cyril@129.206.96.102>
2017-05-10 14:33:52 +0200

    Added the service for displaying loading screen on the front end

---
+++ src/main/java/ijfx/ui/loading/LoadingScreenService.java
@@ -0,0 +1,74 @@
+/*
+ * /*
+ *     This file is part of ImageJ FX.
+ *
+ *     ImageJ FX is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     ImageJ FX is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with ImageJ FX.  If not, see <http://www.gnu.org/licenses/>. 
+ *
+ * 	Copyright 2015,2016 Cyril MONGIS, Michael Knop
+ *
+ */
+package ijfx.ui.loading;
+
+import ijfx.core.IjfxService;
+import java.util.function.Consumer;
+import javafx.concurrent.Task;
+import org.scijava.command.CommandService;
+import org.scijava.event.EventService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.service.AbstractService;
+import org.scijava.service.SciJavaService;
+
+/**
+ *
+ * @author Cyril MONGIS, 2015
+ */
+@Plugin(type = SciJavaService.class)
+public class LoadingScreenService extends AbstractService implements IjfxService,Consumer<Task>{
+    
+    @Parameter
+    EventService eventService;
+    
+    
+    @Parameter
+    CommandService commandService;
+   
+    
+    public void frontEndTask(Task task){
+        frontEndTask(task,false);
+    }
+    
+    
+    
+    
+    public void frontEndTask(Task task, boolean canCancel) {
+       eventService.publish(new ForegroundTaskSubmitted().setCancelable(canCancel).setObject(task));
+    }
+    
+    public void backgroundTask(Task task, boolean cancelable) {
+        
+        
+    }
+
+    public void submitTask(Task task) {
+           backgroundTask(task,true);
+    }
+
+    @Override
+    public void accept(Task t) {
+        frontEndTask(t);
+    }
+
+    
+}
