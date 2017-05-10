0dc27c257d40cb3b1bde3e9140f124c64c63c9d1
cyril <cyril@129.206.96.102>
2017-05-10 16:37:02 +0200

    Wraps a MetaDataOwner to an Explorable

---
+++ src/main/java/ijfx/explorer/wrappers/MetaDataSetExplorerWrapper.java
@@ -0,0 +1,104 @@
+/*
+    This file is part of ImageJ FX.
+
+    ImageJ FX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ImageJ FX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ImageJ FX.  If not, see <http://www.gnu.org/licenses/>. 
+    
+     Copyright 2015,2016 Cyril MONGIS, Michael Knop
+	
+ */
+package ijfx.explorer.wrappers;
+
+import ijfx.core.metadata.MetaData;
+import ijfx.core.metadata.MetaDataSet;
+import ijfx.explorer.datamodel.AbstractExplorable;
+import ijfx.explorer.datamodel.Explorable;
+import ijfx.ui.main.ImageJFX;
+import java.util.stream.Stream;
+import javafx.scene.image.Image;
+import net.imagej.Dataset;
+
+/**
+ *
+ * @author Cyril MONGIS, 2016
+ */
+public class MetaDataSetExplorerWrapper extends AbstractExplorable implements Explorable{
+
+    private static Image bieber = new Image("http://www.billboard.com/files/styles/article_main_image/public/media/justin-bieber-shirtless-presents-onstage-at-fashion-rocks-2014-billboard-650.jpg");
+    
+    private final MetaDataSet wrappedMetaDataSet;
+    
+    
+    public MetaDataSetExplorerWrapper(MetaData... metaDatas) {
+        this(new MetaDataSet());
+        Stream
+                .of(metaDatas)
+                .forEach(getMetaDataSet()::put);
+                
+        
+    }
+    
+    public MetaDataSetExplorerWrapper(MetaDataSet metaDataSet) {
+        wrappedMetaDataSet = metaDataSet;
+    }
+
+    
+    @Override
+    public String getTitle() {
+        String value =  getMetaDataSet().get(MetaData.FILE_NAME).getStringValue();
+        
+        if(value == null) {
+            return getMetaDataSet()
+                    .getOrDefault(MetaData.NAME,MetaData.create(MetaData.NAME, "Embarassing..."))
+                    .getStringValue();
+        }
+        return "Embarassing...";
+    }
+
+    @Override
+    public String getSubtitle() {
+        return "";
+    }
+
+    @Override
+    public String getInformations() {
+        return "";
+    }
+
+    @Override
+    public Image getImage() {
+        return bieber;
+    }
+
+    @Override
+    public void open() throws Exception {
+        ImageJFX.getLogger().severe("Not supported.");
+    }
+
+    
+
+    @Override
+    public Dataset getDataset() {
+        return null;
+    }
+    
+    @Override
+    public MetaDataSet getMetaDataSet() {
+        return wrappedMetaDataSet;
+    }
+    
+    public void dispose() {
+        
+    }
+    
+}
