8b0e3c5b1d738967aef7664361bb0cf8d2ca2a53
cyril <cyril@129.206.96.102>
2017-05-14 09:07:20 +0200

    Added ChannelSettings related classes

---
+++ src/main/java/ijfx/core/image/ChannelSettings.java
@@ -0,0 +1,75 @@
+/*
+    This file is part of ImageJ FX.
+
+    ImageJ FX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ImageJ FX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ImageJ FX.  If not, see <http://www.gnu.org/licenses/>. 
+    
+     Copyright 2015,2016 Cyril MONGIS, Michael Knop
+	
+ */
+package ijfx.core.image;
+
+import java.util.List;
+import net.imagej.Dataset;
+import net.imagej.display.DatasetView;
+
+/**
+ *
+ * @author cyril
+ */
+public interface ChannelSettings {
+
+    public int getChannelCount();
+
+    public List<Channel> getChannelSettings();
+
+    public void setChannels(List<Channel> settings);
+
+    public default Channel get(int i) {
+        return getChannelSettings().get(i);
+    }
+
+    public static void applyTo(ChannelSettings set, Dataset dataset) {
+        if (set != null) {
+            for (int i = 0; i != set.getChannelCount(); i++) {
+                // applying the settings to the dataset
+                set.get(i).apply(dataset, i);
+            }
+        }
+
+    }
+
+    public default void apply(DatasetView datasetView) {
+
+        for (int i = 0; i != datasetView.getChannelCount(); i++) {
+
+            if (i + 1 > getChannelCount()) {
+                return;
+            }
+            get(i).apply(datasetView, i);
+
+        }
+
+    }
+
+    public default void apply(Dataset dataset) {
+        for (int i = 0; i != dataset.getChannels(); i++) {
+            if (i + 1 > getChannelCount()) {
+                return;
+            }
+            get(i).apply(dataset, i);
+
+        }
+    }
+
+}
