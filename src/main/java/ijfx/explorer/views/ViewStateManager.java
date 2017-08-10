3d8eaba6b796efbcfc5bf35c56468beed764e72c
cyril <cyril@129.206.96.102>
2017-08-10 12:35:18 +0200

    ViewStateManager now takes care of updating the filter panel

--- src/main/java/ijfx/explorer/views/ViewStateManager.java
+++ src/main/java/ijfx/explorer/views/ViewStateManager.java
@@ -23,92 +23,116 @@ import ijfx.explorer.ExplorableViewModel;
 import ijfx.explorer.ExplorableList;
 import ijfx.explorer.datamodel.Explorable;
 import ijfx.explorer.views.ViewStateManager.ViewState;
+import ijfx.ui.filters.metadata.TaggableFilterPanel;
 import java.util.List;
 import java.util.WeakHashMap;
+import mongis.utils.ProgressHandler;
 
 /**
- * Object responsible of synchronize the ExplorableViewModel state
- * with view states.
+ * Object responsible of synchronize the ExplorableViewModel state with view
+ * states.
+ *
  * @author cyril
  */
-public class ViewStateManager extends WeakHashMap<ExplorerView, ViewState>{
-    
-    
-    
-    
+public class ViewStateManager extends WeakHashMap<ExplorerView, ViewState> {
+
     private static int NOT_CALCULATED = -10;
-    
-    ViewState displayState;
-    
-    
-    
-    public void updateState(ExplorableViewModel display) {
-        
-        displayState = new ViewState(display);
-        
+
+    private ViewState displayState;
+
+    private int itemState = NOT_CALCULATED;
+
+    private TaggableFilterPanel taggleFilterPanel;
+
+    public void setTaggleFilterPanel(TaggableFilterPanel taggleFilterPanel) {
+        this.taggleFilterPanel = taggleFilterPanel;
     }
+
     
-    public void checkView(ExplorerView view, ExplorableViewModel display) {
-        
-        if(containsKey(view) == false) {
-            put(view,new ViewState());
+    
+    
+    public void updateState(ExplorableViewModel model) {
+
+        displayState = new ViewState(model);
+        checkFilterPanel(model);
+
+    }
+
+    
+    
+    public void checkFilterPanel(ExplorableViewModel model) {
+
+        if (taggleFilterPanel != null) {
+
+            int newItemState = ExplorableList.contentHash(model.getItems());
+
+            if (newItemState != this.itemState) {
+
+                itemState = newItemState;
+                taggleFilterPanel.updateFilters(ProgressHandler.check(null), model.getItems());
+            }
+
         }
-        
+    }
+
+    public void checkView(ExplorerView view, ExplorableViewModel display) {
+
+        if (displayState == null || display == null) {
+            return;
+        }
+        if (containsKey(view) == false) {
+            put(view, new ViewState());
+        }
+
         ViewState viewState = get(view);
         ViewState displayState = this.displayState;
         // if the content of the view is differnet of the content
         // of the display, it has to set the items
-       if(viewState.listContent != displayState.listContent) {
-           
-           view.setItems(display.getDisplayedItems());
-           
-       }
-       // otherwise, if the content is the same but the elements
-       // inside where modified
-       else {
-           if(viewState.data != displayState.data) {
-               view.refresh();
-           }
-       }
-       
-       if(displayState.selection != viewState.selection){
-           view.setSelectedItem(display.getSelectedItems());
-       }
-       
-       put(view,displayState.clone());
-       
+        if (viewState.listContent != displayState.listContent) {
+
+            view.setItems(display.getDisplayedItems());
+
+        } // otherwise, if the content is the same but the elements
+        // inside where modified
+        else {
+            if (viewState.displayedData != displayState.displayedData) {
+                view.refresh();
+            }
+        }
+
+        if (displayState.selection != viewState.selection) {
+            view.setSelectedItem(display.getSelectedItems());
+        }
+
+        put(view, displayState.clone());
+
     }
-    
-    
-    
+
     public class ViewState {
-        
+
         int listContent = NOT_CALCULATED;
-                
-        int data = NOT_CALCULATED;
+
+        int displayedData = NOT_CALCULATED;
 
         int selection = NOT_CALCULATED;
 
         public ViewState() {
-            
+
         }
-        
+
         public ViewState(ExplorableViewModel display) {
             listContent = ExplorableList.listHash(display.getDisplayedItems());
-            data = ExplorableList.contentHash(display.getDisplayedItems());
-            selection = ExplorableList.listHashWithOrder(display.getSelectedItems());        
+            displayedData = ExplorableList.contentHash(display.getDisplayedItems());
+            selection = ExplorableList.listHashWithOrder(display.getSelectedItems());
         }
-        
+
         public ViewState clone() {
             ViewState state = new ViewState();
-            state.data = data;
+            state.displayedData = displayedData;
             state.selection = selection;
             state.listContent = listContent;
             return state;
         }
     }
-    
-    
-    
-    
+
 }
