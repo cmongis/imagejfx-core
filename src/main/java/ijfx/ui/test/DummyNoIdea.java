c068e92c382d415e56d155983352b113d0891dca
SapholFaire <salut.a.vous@hotmail.fr>
2017-08-17 16:46:04 +0200

    No display, work in progress

--- src/main/java/ijfx/ui/test/DummyNoIdea.java
+++ src/main/java/ijfx/ui/test/DummyNoIdea.java
@@ -19,42 +19,87 @@
  */
 package ijfx.ui.test;
 
+import ijfx.core.metadata.MetaData;
+import ijfx.explorer.ExplorableList;
 import ijfx.explorer.datamodel.Explorable;
+import ijfx.explorer.datamodel.Tag;
+import ijfx.explorer.wrappers.MetaDataSetExplorerWrapper;
 import ijfx.ui.utils.CategorizedExplorableController;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 import javafx.scene.layout.Pane;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.scijava.ItemIO;
 import org.scijava.command.Command;
 import org.scijava.command.ContextCommand;
+import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
 /**
  *
  * @author sapho
  */
-@Plugin(type = Command.class, menuPath = "Plugins > Test > Generate Dummy Something")
+@Plugin(type = Command.class, menuPath = "Plugins > Test > Generate Dummy No Idea")
 public class DummyNoIdea extends ContextCommand {
 
     public CategorizedExplorableController ctrl = new CategorizedExplorableController();
 
+    @Parameter(type = ItemIO.OUTPUT)
+    ExplorableList output;
+
+    @Parameter(type = ItemIO.OUTPUT)
+    ExplorableList output2;
+
     @Override
     public void run() {
-        
-        List <Explorable> truc = new ArrayList<>();
 
-        
+        ExplorableList output = new ExplorableList();
+        ExplorableList output2 = new ExplorableList();
+
+        for (int i = 0; i != 10; i++) {
+
+            MetaData name = MetaData.create(MetaData.NAME, RandomStringUtils.random(3, true, false));
+            MetaData m1 = MetaData.create("Random strings 1", RandomStringUtils.random(5, true, false));
+            MetaData m2 = MetaData.create("Random strings 2", RandomStringUtils.random(3, true, false));
+            MetaData m3 = MetaData.create("Random double 1", new Random().nextDouble());
+            MetaData m4 = MetaData.create("Random double 2", new Random().nextDouble());
+
+            Explorable explorable = new MetaDataSetExplorerWrapper(name, m1, m2, m3, m4);
+            explorable.addTag(Tag.create(RandomStringUtils.random(3, true, false)));
+            explorable.addTag(Tag.create(RandomStringUtils.random(3, true, false)));
+            output.add(explorable);
+
+        }
+
+        for (int i = 0; i != 10; i++) {
+
+            MetaData name = MetaData.create(MetaData.NAME, RandomStringUtils.random(3, true, false));
+            MetaData m1 = MetaData.create("Random strings 1", RandomStringUtils.random(5, true, false));
+            MetaData m2 = MetaData.create("Random strings 2", RandomStringUtils.random(3, true, false));
+            MetaData m3 = MetaData.create("Random double 1", new Random().nextDouble());
+            MetaData m4 = MetaData.create("Random double 2", new Random().nextDouble());
+
+            Explorable explorable = new MetaDataSetExplorerWrapper(name, m1, m2, m3, m4);
+            explorable.addTag(Tag.create(RandomStringUtils.random(3, true, false)));
+            explorable.addTag(Tag.create(RandomStringUtils.random(3, true, false)));
+            output2.add(explorable);
+
+        }
+
         Pane pane = ctrl
                 .addCategory("Cat 1")
-                .setElements("Cat 1", truc) //set the element that the first line will contain
+                .setElements("Cat 1", output) //set the element that the first line will contain
                 .addCategory("Cat 2")
-                .setElements("Cat 2",truc)
-                .setMaxItemPerCategory(5)
+                .setElements("Cat 2", output2)
+                //.setMaxItemPerCategory(5)
                 .generate(); // returns the Pane that contains the view
+        
+        
 
-        //ctrl.setElements("Cat 2", truc);
-        ctrl.update(); // updates the pane returned previously
-
+        //ctrl.setElements("Cat 2", truc2);
+        //ctrl.update(); // updates the pane returned previously
+        
+        //pane.getParent();
+        
     }
 
 }
