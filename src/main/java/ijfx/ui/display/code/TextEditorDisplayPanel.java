dce49ba130dfc84abcb62856a9dcf8f5a2cecfce
florianlassalle <nairolfdix@gmail.com>
2017-08-18 18:04:47 +0200

    add a console view to the text display panel

--- src/main/java/ijfx/ui/display/code/TextEditorDisplayPanel.java
+++ src/main/java/ijfx/ui/display/code/TextEditorDisplayPanel.java
@@ -24,24 +24,35 @@ import ijfx.ui.display.image.AbstractFXDisplayPanel;
 import ijfx.ui.display.image.FXDisplayPanel;
 import ijfx.ui.main.ImageJFX;
 import java.lang.reflect.Field;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
+import java.util.stream.Collectors;
 import javafx.application.Platform;
 import javafx.beans.Observable;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import org.scijava.command.CommandService;
+import org.scijava.console.ConsoleService;
+import org.scijava.console.OutputEvent;
 import org.scijava.event.EventHandler;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.script.ScriptLanguage;
 import org.scijava.script.ScriptService;
 import org.scijava.ui.viewer.DisplayWindow;
+import org.scijava.ui.console.ConsolePane;
+import rx.schedulers.Schedulers;
+import rx.subjects.PublishSubject;
 
 /**
  * TODO : Change to ScriptDisplayPanelFX
@@ -50,7 +61,7 @@ import org.scijava.ui.viewer.DisplayWindow;
  * @author Cyril MONGIS (contribution)
  */
 @Plugin(type = FXDisplayPanel.class)
-public class TextEditorDisplayPanel extends AbstractFXDisplayPanel<ScriptDisplay> {
+public class TextEditorDisplayPanel extends AbstractFXDisplayPanel<ScriptDisplay> implements ConsolePane<Node>{
 
     @Parameter
     private ScriptService scriptService;
@@ -63,6 +74,9 @@ public class TextEditorDisplayPanel extends AbstractFXDisplayPanel<ScriptDisplay
 
     @Parameter
     private UiCommandService uiCommandSrv;
+    
+    @Parameter
+    ConsoleService consoleService;
 
     private DefaultTextArea textArea;
 
@@ -73,6 +87,22 @@ public class TextEditorDisplayPanel extends AbstractFXDisplayPanel<ScriptDisplay
 
     @FXML
     private Button runButton;
+    
+    @FXML
+    private SplitPane splitPane;
+    
+    @FXML
+    private Button consoleButton;
+    
+    @FXML
+    private Button vButton;
+    
+    @FXML
+    private Button hButton;
+    
+    PublishSubject<String> consoleDispatcher = PublishSubject.create();
+    
+    private TextArea consoleArea;
 
     public TextEditorDisplayPanel() {
         super(ScriptDisplay.class);
@@ -96,8 +126,20 @@ public class TextEditorDisplayPanel extends AbstractFXDisplayPanel<ScriptDisplay
 
             DefaultTextArea textArea = getTextArea();
 
-            root.setCenter(textArea);
-
+            splitPane.getItems().add(0, textArea);
+            
+            this.consoleArea = new TextArea();
+            consoleService.addOutputListener(this);
+            
+            consoleDispatcher
+                .observeOn(Schedulers.from(ImageJFX.getThreadPool()))
+                .buffer(1000, TimeUnit.MILLISECONDS)
+                
+                .filter(strList->!strList.isEmpty())
+                .map(strList->strList.stream().collect(Collectors.joining()))
+                .subscribe(str->Platform.runLater(()->consoleArea.appendText(str)));
+            
+            //this.consoleArea.getStylesheets().add(this.getClass().getResource("bijou.min.css").toExternalForm());
         
             textArea.setBottomAnchor(textArea.getCodeArea(), 15d);
             textArea.setTopAnchor(textArea.getCodeArea(), 0d);
@@ -138,9 +180,37 @@ public class TextEditorDisplayPanel extends AbstractFXDisplayPanel<ScriptDisplay
 
     @FXML
     public void runScript() {
+        this.consoleArea.clear();
         getDisplay().runScript();
     }
-
+    
+    
+    @FXML
+    public void flipConsoleVertical() {
+        this.splitPane.setOrientation(Orientation.HORIZONTAL);
+    }
+    
+    @FXML
+    public void flipConsoleHorizontal() {
+        this.splitPane.setOrientation(Orientation.VERTICAL);
+    }
+    
+     @FXML
+    public void showConsole() {
+        if (this.splitPane.getItems().size() >= 2){
+            this.splitPane.getItems().remove(1);
+        }
+        else {
+            this.splitPane.getItems().add(this.consoleArea);
+            initConsole();
+        }
+    }
+    
+    public void initConsole(){
+        this.consoleArea.clear();
+        
+    }
+    
     public void initCode() {
         Platform.runLater(() -> {
             getTextArea().setText(getDisplay().get(0).getCode());
@@ -201,6 +271,30 @@ public class TextEditorDisplayPanel extends AbstractFXDisplayPanel<ScriptDisplay
     public void onRedoEvent(RedoEvent event) {
         this.textArea.redo();
 
+    }
+
+    @Override
+    public void append(OutputEvent event) {
+        consoleDispatcher.onNext(event.getOutput()); 
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public Node getComponent() {
+        return consoleArea;
+    }
+
+    @Override
+    public Class<Node> getComponentType() {
+        return Node.class;
+    }
+
+    @Override
+    public void outputOccurred(OutputEvent event) {
+        append(event);
     }
 
 }
