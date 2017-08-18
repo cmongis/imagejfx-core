8d15026d72297f8825b3ffa867d3d280488ae670
SapholFaire <salut.a.vous@hotmail.fr>
2017-08-18 16:31:01 +0200

    Clean code

--- src/main/java/ijfx/ui/test/DummyNoIdea.java
+++ src/main/java/ijfx/ui/test/DummyNoIdea.java
@@ -45,7 +45,7 @@ import org.scijava.plugin.Plugin;
  *
  * @author sapho
  */
-@Plugin(type = Command.class, menuPath = "Plugins > Test > Generate Dummy No Idea")
+@Plugin(type = Command.class, menuPath = "Plugins > Test > Generate Dummy Categories")
 public class DummyNoIdea extends ContextCommand {
 
     public CategorizedExplorableController ctrl = new CategorizedExplorableController();
@@ -56,11 +56,15 @@ public class DummyNoIdea extends ContextCommand {
     @Parameter(type = ItemIO.OUTPUT)
     ExplorableList output2;
 
+    @Parameter(type = ItemIO.OUTPUT)
+    ExplorableList output3;
+
     @Override
     public void run() {
 
         ExplorableList output = new ExplorableList();
         ExplorableList output2 = new ExplorableList();
+        ExplorableList output3 = new ExplorableList();
 
         for (int i = 0; i != 10; i++) {
 
@@ -92,42 +96,43 @@ public class DummyNoIdea extends ContextCommand {
 
         }
 
+        for (int i = 0; i != 5; i++) {
+
+            MetaData name = MetaData.create(MetaData.NAME, RandomStringUtils.random(3, true, false));
+            MetaData m1 = MetaData.create("Random strings 1", RandomStringUtils.random(5, true, false));
+            MetaData m2 = MetaData.create("Random strings 2", RandomStringUtils.random(3, true, false));
+            MetaData m3 = MetaData.create("Random double 1", new Random().nextDouble());
+            MetaData m4 = MetaData.create("Random double 2", new Random().nextDouble());
+
+            Explorable explorable = new MetaDataSetExplorerWrapper(name, m1, m2, m3, m4);
+            explorable.addTag(Tag.create(RandomStringUtils.random(3, true, false)));
+            explorable.addTag(Tag.create(RandomStringUtils.random(3, true, false)));
+            output3.add(explorable);
+
+        }
+
         Pane pane = ctrl
                 .addCategory("Cat 1")
                 .setElements("Cat 1", output) //set the element that the first line will contain
                 .addCategory("Cat 2")
                 .setElements("Cat 2", output2)
-                //.setMaxItemPerCategory(5)
+                .setMaxItemPerCategory(5)
                 .generate(); // returns the Pane that contains the view
+
+        ctrl.setElements("Cat 2", output3);
+        ctrl.update(); // updates the pane returned previously
 
         Platform.runLater(() -> {
 
             Stage stage = new Stage();
             stage.initModality(Modality.APPLICATION_MODAL);
-            Label label = new Label("truc");
-            pane.getChildren().add(label);
-            Scene scene = new Scene(pane, 200, 100);
+            Scene scene = new Scene(pane, 350, 500);
+
             stage.setScene(scene);
             stage.show();
-            /*
 
-
-            Alert alert = new Alert(AlertType.ERROR);
-            alert.setTitle("Information Dialog");
-            //alert.setGraphic(pane);
-            //alert.getDialogPane().setContent(pane);
-            //alert.getDialogPane().setExpandableContent(pane);
-            //alert.getDialogPane().setVisible(true);
-            alert.getDialogPane().getChildren().add(pane);
-            alert.getDialogPane().setPrefSize(480, 320);
-
-            alert.showAndWait();
-*/
         });
 
-        //ctrl.setElements("Cat 2", truc2);
-        //ctrl.update(); // updates the pane returned previously
-        //pane.getParent();
     }
 
 }
