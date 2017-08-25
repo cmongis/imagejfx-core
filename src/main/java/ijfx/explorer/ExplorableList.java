8472b7c5146ca4827d16e0618bbff5dc229ab21a
cyril <cyril@129.206.96.102>
2017-08-25 10:43:06 +0200

    Added new filters

--- src/main/java/ijfx/explorer/ExplorableList.java
+++ src/main/java/ijfx/explorer/ExplorableList.java
@@ -23,81 +23,117 @@ import ijfx.explorer.datamodel.Explorable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.IntUnaryOperator;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
  *
  * @author cyril
  */
-public class ExplorableList extends ArrayList<Explorable>{
-    
+public class ExplorableList extends ArrayList<Explorable> {
+
     public ExplorableList() {
         super();
     }
-    
-   
+
     public ExplorableList(List<? extends Explorable> list) {
         super(list.size());
         addAll(list);
     }
-    
-    
-   
-    
-    
+
+    public ExplorableList filter(Predicate<? super Explorable> predicate) {
+
+        return new ExplorableList(
+                this
+                        .stream()
+                        .filter(item -> !predicate.test(item))
+                        .collect(Collectors.toList())
+        );
+
+    }
+
+    public ExplorableList filterGreaterThan(String key, double minValue) {
+        return filter(exp -> exp.getMetaDataSet().get(key).getDoubleValue() > minValue);
+    }
+
     /**
-     * Return a hash that only takes account the which elements are
-     * in the list
+     * Return a hash that only takes account the which elements are in the list
+     *
      * @return hash
      */
     public static int listHash(Collection<? extends Explorable> list) {
-     if(list == null) return 0;
-        
-        return list
+        if (list == null) {
+            return 0;
+        }
+
+        return new ArrayList(list)
                 .stream()
-                .mapToInt(exp->exp.hashCode())
-                
+                .mapToInt(exp -> exp.hashCode())
                 .parallel()
                 .sum();
-    
+
     }
-    
-   
-    
+
     public static int listHashWithOrder(List<? extends Explorable> list) {
-        
-        if(list == null) return 0;
-        
+
+        if (list == null) {
+            return 0;
+        }
+        final ArrayList<Explorable> arrayList = new ArrayList<Explorable>(list);
+
+        final IntUnaryOperator f = i -> {
+            Explorable exp = arrayList.get(i);
+            return exp == null ? (i + 1) * -1 : exp.hashCode() * (i + 1);
+        };
+
         return IntStream
-                
                 .range(0, list.size())
-                .map(i->list.get(i) == null ? (i+1) * -1 : list.get(i).hashCode() * (i+1))
+                .map(f)
+                // .map(i->arrayList.get(i) == null ? (i+1) * -1 : arrayList.get(i).hashCode() * (i+1))
                 .sum();
-        
+
     }
-    
-     public static int contentHash(Collection<? extends Explorable> list) {
-     if(list == null) return 0;
+
+    public static int contentHash(Collection<? extends Explorable> collection) {
+        if (collection == null) {
+            return 0;
+        }
+        
+        List<Explorable> list = new ArrayList(collection);
         
         return list
                 .stream()
-                .mapToInt(exp->exp.dataHashCode())
+                .mapToInt(exp -> exp.dataHashCode())
                 .parallel()
                 .sum();
-    
+
     }
-    
-    
+
     public static int contentHashWidthOrder(List<? extends Explorable> list) {
-        
-        if(list == null) return 0;
-        
+
+        if (list == null) {
+            return 0;
+        }
+        final ArrayList<Explorable> arrayList = new ArrayList<Explorable>(list);
+
+        final IntUnaryOperator f = i -> {
+            Explorable exp = arrayList.get(i);
+            return exp == null ? (i + 1) * -1 : exp.dataHashCode() * (i + 1);
+        };
+
         return IntStream
-                
                 .range(0, list.size())
-                .map(i->list.get(i) == null ? (i+1) * -1 : list.get(i).hashCode() * (i+1))
-                .sum();  
+                .map(f)
+                // .map(i->arrayList.get(i) == null ? (i+1) * -1 : arrayList.get(i).hashCode() * (i+1))
+                .sum();
+        
+        
+       
     }
     
     
+
 }
