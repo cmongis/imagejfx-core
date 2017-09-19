2e0ef2dfeaa8c541ce046eb94d6cf12f6a98dc5d
cyril <cyril@129.206.96.102>
2017-09-19 11:52:02 +0200

    Added comments

--- src/main/java/install-ijfx.js
+++ src/main/java/install-ijfx.js
@@ -1,18 +1,29 @@
 //@AppService appService
 //@CommandService commandService
 //@UIService uiService
-var FilesCollection = Java.type("net.imagej.updater.FilesCollection");
+
+
+
+
+// retrieve Fiji directory
 var fiji = appService.getApp().getBaseDirectory();
+
+// load Update Site configuration
+var FilesCollection = Java.type("net.imagej.updater.FilesCollection");
 var filesCollection = new FilesCollection(fiji);
 
 var repoName = "ImageJ-FX";
 var repoUrl = "http://localhost:8080/";
 
-var updateSite = filesCollection.addUpdateSite(repoName, "http://localhost:8080/", null, null, 0);
+// adds ImaegJ-FX update site, activates it and saves the new configuration
+var updateSite = filesCollection.addUpdateSite(repoName, repoUrl, null, null, 0);
 filesCollection.addUpdateSite(updateSite);
 filesCollection.markForUpdate(repoName);
 updateSite.setActive(true);
 filesCollection.write();
+
+// shows confirmation dialog
 uiService.showDialog("ImageJ-FX Update site install. Now proceeding to download.");
 
+// runs update :-)
 commandService.run("net.imagej.ui.swing.updater.ImageJUpdater",true);
