1e292e2a2e9642c6cbdbf509a1865b527edc4e9f
cyril <cyril@129.206.96.102>
2017-09-19 11:52:21 +0200

    Added command to deactivate ImageJ-FX

---
+++ src/main/java/ijfx/ui/main/DeactivateImageJFX.java
@@ -0,0 +1,54 @@
+/*
+    This file is part of ImageJ FX.
+
+    ImageJ FX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ImageJ FX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ImageJ FX.  If not, see <http://www.gnu.org/licenses/>. 
+    
+     Copyright 2015,2016 Cyril MONGIS, Michael Knop
+	
+ */
+package ijfx.ui.main;
+
+import org.scijava.command.Command;
+import org.scijava.command.ContextCommand;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.ui.UIService;
+import org.scijava.ui.swing.SwingUI;
+
+/**
+ *
+ * @author cyril
+ */
+ @Plugin(type = Command.class, menuPath="Help > Switch back to ImageJ/Fiji")
+public class DeactivateImageJFX extends ContextCommand{
+   
+
+    @Parameter
+    UIService uiService;
+    
+    @Override
+    public void run() {
+        
+        uiService.getUI(SwingUI.NAME).dispose();
+        
+        uiService.setDefaultUI(uiService.getUI(SwingUI.NAME));
+        
+        
+        //uiService.showUI(ImageJFX.UI_NAME);
+        
+        
+        
+    }
+    
+}
