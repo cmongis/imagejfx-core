cb0e26091eaa7fbb33e6199b6ac2c03cf2c829b5
cyril <cyril@129.206.96.102>
2017-09-28 15:50:08 +0200

    Added new command to save any list of explorable into a explorable database

---
+++ src/main/java/ijfx/commands/explorable/SaveAsDatabase.java
@@ -0,0 +1,62 @@
+/*
+    This file is part of ImageJ FX.
+
+    ImageJ FX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ImageJ FX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ImageJ FX.  If not, see <http://www.gnu.org/licenses/>. 
+    
+     Copyright 2015,2016 Cyril MONGIS, Michael Knop
+	
+ */
+package ijfx.commands.explorable;
+
+import ijfx.core.io.ExplorableIOService;
+import ijfx.explorer.datamodel.Explorable;
+import java.io.File;
+import java.util.List;
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.ui.DialogPrompt;
+import org.scijava.ui.UIService;
+import org.scijava.widget.FileWidget;
+
+/**
+ *
+ * @author cyril
+ */
+@Plugin(type = ExplorableDisplayCommand.class, label = "Save database",iconPath = "fa:save")
+public class SaveAsDatabase extends AbstractExplorableDisplayCommand{
+
+    
+    @Parameter(label = "Select output file",style = FileWidget.SAVE_STYLE + " " + ExplorableIOService.DB_EXTENSION)
+    File outputFile;
+    
+    @Parameter
+    ExplorableIOService explorableIOService;
+    
+   
+    
+    @Override
+    public void run(List<? extends Explorable> items) throws Exception {
+        
+        if(outputFile == null) return;
+        
+        
+        
+        explorableIOService.saveAll(items, outputFile);
+        
+        uiService.showDialog("Database exported successfully", DialogPrompt.MessageType.INFORMATION_MESSAGE);
+        
+    }
+    
+}
