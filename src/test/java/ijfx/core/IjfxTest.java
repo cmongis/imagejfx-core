d27d0e06ec844bded92ff2d057fb194fe0b47b2e
cyril <cyril@129.206.96.102>
2017-09-28 15:46:32 +0200

    displayFile method now detect compressed text files and takes care of decompressing them to display them

--- src/test/java/ijfx/core/IjfxTest.java
+++ src/test/java/ijfx/core/IjfxTest.java
@@ -19,54 +19,75 @@
  */
 package ijfx.core;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.zip.GZIPInputStream;
 import net.imagej.ImageJ;
 import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.scijava.Context;
 import org.scijava.plugin.Parameter;
-
+import static scala.xml.XML.encoding;
 
 /**
  *
  * @author Cyril MONGIS
  */
-
 public abstract class IjfxTest {
-    
+
     private static ImageJ imageJ;
-    
+
     private static boolean injected = false;
-    
+
     @Parameter
     Context context;
-    
+
     protected ImageJ getImageJ() {
-        
-        if(imageJ == null) {
+
+        if (imageJ == null) {
             imageJ = new ImageJ();
         }
         return imageJ;
-        
+
     }
-    
-    
+
     @Before
     public void injectFirst() {
-        if(injected == false) {
+        if (injected == false) {
             System.out.println("Injecting SciJava Context");
             getImageJ().context().inject(this);
         }
     }
-    
+
     public Context getContext() {
         return context;
     }
-    
-    
-     public void displayFile(File file) throws IOException{
+
+    public void displayFile(File file) throws IOException {
+
         System.out.println(file.toString());
-         System.out.println(FileUtils.readFileToString(file));
+
+        if (file.getName().endsWith(".gz")) {
+
+            FileInputStream fis = new FileInputStream(file);
+
+            GZIPInputStream gis = new GZIPInputStream(fis);
+
+            Reader decoder = new InputStreamReader(gis, "utf-8");
+            BufferedReader buffered = new BufferedReader(decoder);
+            String line = buffered.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = buffered.readLine();
+            }
+
+        } else {
+
+            System.out.println(FileUtils.readFileToString(file));
+        }
     }
 }
