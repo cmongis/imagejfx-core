7e47c4caf745ca5ba35bb90492bb22ed0ed3e311
cyril <cyril@129.206.96.102>
2017-10-02 14:37:43 +0200

    fixed loading of pixels from a saved segmented object

--- src/main/java/ijfx/core/segmentation/DefaultSegmentedObject.java
+++ src/main/java/ijfx/core/segmentation/DefaultSegmentedObject.java
@@ -24,19 +24,23 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import ijfx.core.image.DatasetUtilsService;
+import ijfx.core.image.ImagePlaneService;
 import ijfx.core.metadata.MetaData;
 import ijfx.core.metadata.MetaDataSet;
 import ijfx.core.metadata.MetaDataSetType;
+import ijfx.core.metadata.MetaDataSetUtils;
 import ijfx.core.overlay.OverlayStatService;
 import ijfx.core.overlay.OverlayStatistics;
 import ijfx.explorer.datamodel.AbstractTaggable;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import net.imagej.Dataset;
 import net.imagej.overlay.Overlay;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.numeric.RealType;
 import org.scijava.plugin.Parameter;
+
 /**
  *
  * @author Cyril MONGIS, 2016
@@ -46,80 +50,73 @@ public class DefaultSegmentedObject extends AbstractTaggable implements Segmente
 
     @Parameter
     DatasetUtilsService datasetUtilsService;
-    
+
+    @Parameter
+    ImagePlaneService imagePlaneService;
+
     RandomAccessibleInterval<? extends RealType> source;
     private Overlay overlay;
     private MetaDataSet set = new MetaDataSet();
-    
-    
+
     @Parameter
     private OverlayStatService overlayStatsService;
-    
-    
+
     public DefaultSegmentedObject() {
         set.setType(MetaDataSetType.OBJECT);
     }
-    
-    
 
     @JsonSetter(value = "overlay")
     public void setOverlay(Overlay overlay) {
         this.overlay = overlay;
         set.putGeneric(MetaData.NAME, overlay.getName());
     }
-    
+
     @Override
     @JsonGetter(value = "overlay")
     public Overlay getOverlay() {
         return overlay;
     }
-    
-    
+
     @JsonSetter(value = "metadataset")
     public void setMetaDataSet(MetaDataSet set) {
         this.set = set;
     }
-    
-    
+
     @Override
     @JsonSetter("metadataset")
     public MetaDataSet getMetaDataSet() {
         return set;
     }
-    
+
     public DefaultSegmentedObject(Overlay overlay, OverlayStatistics overlayStatistics) {
         this();
         overlay.getContext().inject(this);
         setOverlay(overlay);
-        
+
         set.merge(overlayStatsService.getStatisticsAsMap(overlayStatistics));
-        
+
     }
 
     @JsonIgnore
     public RandomAccessibleInterval<? extends RealType> getPixelSource() {
-        
-        if(source == null) {
+
+        if (source == null) {
             try {
-                source = datasetUtilsService.openSource(this, true);
+                Dataset dataset = datasetUtilsService.openSource(this, true);
+                long[] nonPlanarPosition = MetaDataSetUtils.getNonPlanarPosition(getMetaDataSet());
+                source = imagePlaneService.planeView(dataset, nonPlanarPosition);
+                
             } catch (IOException ex) {
                 Logger.getLogger(DefaultSegmentedObject.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
+
         return source;
     }
-    
-    
-    
+
     public <T extends RealType<T>> DefaultSegmentedObject setSource(RandomAccessibleInterval<T> interval) {
         this.source = interval;
         return this;
     }
 
-  
-    
-   
-
-   
 }
