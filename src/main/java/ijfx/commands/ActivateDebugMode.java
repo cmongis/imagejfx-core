a4cb35f126cad40e4b5feeef236187920376ad08
cyril <cyril@129.206.96.102>
2017-10-23 15:06:59 +0200

    Added debug mode command

---
+++ src/main/java/ijfx/commands/ActivateDebugMode.java
@@ -0,0 +1,46 @@
+/*
+    This file is part of ImageJ FX.
+
+    ImageJ FX is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ImageJ FX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ImageJ FX.  If not, see <http://www.gnu.org/licenses/>. 
+    
+     Copyright 2015,2016 Cyril MONGIS, Michael Knop
+	
+ */
+package ijfx.commands;
+
+import ijfx.core.uicontext.UiContextService;
+import org.scijava.command.Command;
+import org.scijava.command.ContextCommand;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+/**
+ *
+ * @author cyril
+ */
+@Plugin(type = Command.class, menuPath = "Plugins >  Test > Debug > De/Activate")
+public class ActivateDebugMode extends ContextCommand{
+    
+    private static final String DEBUG_CONTEXT = "debug";
+    
+    @Parameter
+    UiContextService uiContextService;
+
+    @Override
+    public void run() {
+        uiContextService.toggleContext(DEBUG_CONTEXT, !uiContextService.isCurrent(DEBUG_CONTEXT));
+    }
+    
+    
+}
